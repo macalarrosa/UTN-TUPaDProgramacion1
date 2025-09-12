76a8aef0774597a9cee8b2cee12e1363d75e0e56
macalarrosa <macalarrosas2@gmail.com>
2025-09-11 22:19:21 -0300

    Delete unidad1/ejercicio1.py

--- unidad1/ejercicio1.py
+++
@@ -1,6 +0,0 @@
-#1) Escribir un programa que solicite la edad del usuario. Si el usuario es mayor de 18 años, deberá mostrar un mensaje en pantalla que diga “Es mayor de edad”.#
-edad= int (input("Hola! Ingrese su edad por favor: "))
-if edad>= 18:
-    print ("Es mayor de edad")
-else :
-    print (" Es menor de edad")
