45bfc3faf88fd90ef589493170e9617af3508c27
macalarrosa <macalarrosas2@gmail.com>
2025-09-11 22:18:29 -0300

    Delete unidad1/ejercicio5.py

--- unidad1/ejercicio5.py
+++
@@ -1,7 +0,0 @@
-#Escribir un programa que permita introducir contraseñas de entre 8 y 14 caracteres(incluyendo 8 y 14).
-contraseña = str(input("Escriba la contraseña"))
-
-if len(contraseña) >= 8 and len(contraseña) <= 14:
-        print("Ha ingresado una contraseña correcta")
-else:
-        print("Por favor, ingrese una contraseña de entre 8 y 14 caracteres")
